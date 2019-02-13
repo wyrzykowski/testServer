bf2f4d3717ed075ca8cec3c0a6e2cf03eca08d5a
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-02-13 15:12:26 +0100

    Inital commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const hbs = require("hbs");
+var app = express(); //making new express app
+const fs = require("fs");
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs"); // w set to perwsze to key co chce ustawić a drugie to value
+
+//MIDDLEWERE
+app.use((req, res, next) => {
+  //when next(); is call application can run again
+  // if i don't call next() here nothing will load!
+  var now = new Date().toString();
+
+  console.log(`${now}: ${req.method} ${req.url}`);
+  let log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile("server.log", log + "\r\n", err => {
+    if (err) {
+      console.log("Unable to apped to server.log");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
+
+app.use(express.static(__dirname + "/public")); // to get static path to file, __dirname is actual directrory of project
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+}); // It's use to use in partial function definated here
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    helloMessage: "Hello on Home Page!"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({ errorMessage: "Unable to hande request!" });
+});
+
+//This below can run without second argument
+app.listen(3000, () => {
+  console.log("server is up on port 3000");
+});
